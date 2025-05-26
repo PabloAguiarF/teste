fe89326acd0d4ae0e25e1e9775beef675f28a597
PabloAguiarF <154709605+PabloAguiarF@users.noreply.github.com>
2025-05-26 08:21:12 -0300

    Update dashboard_streamlit.py

--- Projeto_Demandas/dashboard_streamlit.py
+++ Projeto_Demandas/dashboard_streamlit.py
@@ -2,13 +2,13 @@ import streamlit as st
 import pandas as pd
 import matplotlib.pyplot as plt
 import numpy as np
-#import locale
+import locale
 
-# Configura o locale para Português Brasil
-#try:
-#    locale.setlocale(locale.LC_ALL, 'pt_BR.UTF-8')
-#except:
-#    locale.setlocale(locale.LC_ALL, 'Portuguese_Brazil.1252')
+ Configura o locale para Português Brasil
+try:
+    locale.setlocale(locale.LC_ALL, 'pt_BR.UTF-8')
+except:
+    locale.setlocale(locale.LC_ALL, 'Portuguese_Brazil.1252')
 
 
 @st.cache_data
